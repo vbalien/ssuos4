46c72771b53a2e790ecfb2f0c4645f4b7c4dfe17
Jisu Kim <webmaster@alien.moe>
2020-11-04 17:30:04 +0900

    2번째 완료

--- 2/rw_lock-w-test.c
+++ 2/rw_lock-w-test.c
@@ -1,26 +1,59 @@
 #include "rw_lock.h"
 
-void init_rwlock(struct rw_lock * rw)
-{
+void init_rwlock(struct rw_lock *rw) {
   //	Write the code for initializing your read-write lock.
+  rw->r_active = rw->w_active = 0;
+  rw->r_wait = rw->w_wait = 0;
+
+  pthread_mutex_init(&rw->mtx, NULL);
+  pthread_cond_init(&rw->read, NULL);
+  pthread_cond_init(&rw->write, NULL);
 }
 
-void r_lock(struct rw_lock * rw)
-{
+void r_lock(struct rw_lock *rw) {
   //	Write the code for aquiring read-write lock by the reader.
+  pthread_mutex_lock(&rw->mtx);
+  if (rw->w_active || rw->w_wait > 0) {
+    rw->r_wait++;
+    while (rw->w_active || rw->w_wait > 0)
+      pthread_cond_wait(&rw->read, &rw->mtx);
+    rw->r_wait--;
+  }
+  rw->r_active++;
+  pthread_mutex_unlock(&rw->mtx);
 }
 
-void r_unlock(struct rw_lock * rw)
-{
+void r_unlock(struct rw_lock *rw) {
   //	Write the code for releasing read-write lock by the reader.
+  pthread_mutex_lock(&rw->mtx);
+  rw->r_active--;
+  if (rw->r_active == 0 && rw->w_wait > 0)
+    pthread_cond_signal(&rw->write);
+  pthread_mutex_unlock(&rw->mtx);
 }
 
-void w_lock(struct rw_lock * rw)
-{
+void w_lock(struct rw_lock *rw) {
   //	Write the code for aquiring read-write lock by the writer.
+  pthread_mutex_lock(&rw->mtx);
+  if (rw->w_active || rw->r_active > 0) {
+    rw->w_wait++;
+    while (rw->w_active || rw->r_active > 0)
+      pthread_cond_wait(&rw->write, &rw->mtx);
+    rw->w_wait--;
+  }
+  rw->w_active = 1;
+  pthread_mutex_unlock(&rw->mtx);
 }
 
-void w_unlock(struct rw_lock * rw)
-{
+void w_unlock(struct rw_lock *rw) {
   //	Write the code for releasing read-write lock by the writer.
+  pthread_mutex_lock(&rw->mtx);
+  rw->w_active = 0;
+  if (rw->r_wait > 0 || rw->w_wait == 0) {
+    pthread_cond_broadcast(&rw->read);
+  }
+  if (rw->w_wait > 0) {
+    pthread_cond_signal(&rw->write);
+  }
+  pthread_mutex_unlock(&rw->mtx);
 }
--- 2/rw_lock.h
+++ 2/rw_lock.h
@@ -1,17 +1,22 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <unistd.h>
 
-struct rw_lock
-{
-
+struct rw_lock {
+  pthread_mutex_t mtx;
+  pthread_cond_t read;
+  pthread_cond_t write;
+  int r_active;
+  int w_active;
+  int r_wait;
+  int w_wait;
 };
 
-void init_rwlock(struct rw_lock * rw);
-void r_lock(struct rw_lock * rw);
-void r_unlock(struct rw_lock * rw);
-void w_lock(struct rw_lock * rw);
-void w_unlock(struct rw_lock * rw);
-long *max_element(long* start, long* end);
-long *min_element(long* start, long* end);
+void init_rwlock(struct rw_lock *rw);
+void r_lock(struct rw_lock *rw);
+void r_unlock(struct rw_lock *rw);
+void w_lock(struct rw_lock *rw);
+void w_unlock(struct rw_lock *rw);
+long *max_element(long *start, long *end);
+long *min_element(long *start, long *end);
